38d38e82a131242e6e97a5d31a9e773f0e473378
Daniil Stefonishin <stefonishin@gmail.com>
2019-09-02 17:09:59 +0300

    First attempts

---
+++ pic/src/maxwell/maxwell.h
@@ -0,0 +1,24 @@
+#ifndef MAXWELL_H
+#define MAXWELL_H
+
+namespace maxwell {
+
+class Field
+{
+    private:
+
+        // device data
+        void * data;
+        
+    public:
+
+        Field(void);
+        ~Field(void);
+
+        //
+
+};
+
+} // namespace maxwell
+
+#endif // MAXWELL_H
