00abe67aacdf0a6e6729dc0327c41ba8abce264e
Daniil Stefonishin <stefonishin@gmail.com>
2019-09-24 08:41:38 +0300

    Array class

--- pic/src/utility/utility.h
+++ pic/src/utility/utility.h
@@ -9,6 +9,29 @@ void Init_array(const uint, const Type &, Type *&);
 template<typename Type>
 void Init_array(const uint, const Type *, Type *&);
 
+template<typename Type>
+class Array
+{
+    private:
+
+        uint size;
+        uint capacity;
+
+        Type * data;
+
+    public:
+
+        Array();
+        Array(const uint);
+        Array(const uint, const Type &);
+        Array(const uint, const Type *);
+
+        ~Array();
+
+        Type & operator[](const uint ind) { return data[ind]; }
+        const Type & operator[](const uint ind) const { return data[ind]; }
+}
+
 } // namespace maxwell
 
 #endif // UTILITY_H
